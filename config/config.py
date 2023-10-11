26fefe71ed5ea5ad26262fb16806ab08a3ffb393
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2023-10-11 12:19:18 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -130,7 +130,7 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/e811ce6053322bca41daf.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/5ee45e51e1c97b9f5af49.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
